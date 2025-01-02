755eb244d20be2203da3519d00fbbef8b4a60021
Lofy2003 <128834726+Lofy2003@users.noreply.github.com>
2025-01-02 15:24:24 +0300

    second commit

--- moh.js
+++ moh.js
@@ -3,7 +3,7 @@ var myTable = document.getElementById("myTable")
 var condition = 'sent succesfully';
 myButton.addEventListener("click", addProductToTable)
 
-var index=1
+var index = 1
 function addProductToTable() {
     event.preventDefault();
     var conNameInp = document.getElementById("nameCus")
@@ -12,18 +12,17 @@ function addProductToTable() {
     var nameCus = conNameInp.value;
     var emailCus = conEmailInp.value;
 
-    var myTr=document.createElement("tr")
+    var myTr = document.createElement("tr")
+    var myTd1 = document.createElement("td")
+    var myTd2 = document.createElement("td")
+    var myTd3 = document.createElement("td")
+    var myTd4 = document.createElement("td")
 
-    var myTd1=document.createElement("td")
-    var myTd2=document.createElement("td")
-    var myTd3=document.createElement("td")
-    var myTd4=document.createElement("td")
-
-    myTd1.textContent=index;
+    myTd1.textContent = index;
     index++;
-    myTd2.textContent=nameCus;
-    myTd3.textContent=emailCus;
-    myTd4.textContent=condition;
+    myTd2.textContent = nameCus;
+    myTd3.textContent = emailCus;
+    myTd4.textContent = condition;
 
     myTr.appendChild(myTd1)
     myTr.appendChild(myTd2)
